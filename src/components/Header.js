7deffc0f831e24cd74c008cac0b5fbd45ba6de3f
Marcus-Johnson <marcusjdevelops@gmail.com>
2021-02-27 19:42:46 -0600

    button revisions

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,15 @@ import Button from './Button'
 
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='+'/>
+            <Button color='green' text='+' onClick=
+            {onClick} />
         </header>
     )
 }
