02ca52ade38dd1f106d8c85134c094141b879916
Marcus-Johnson <marcusjdevelops@gmail.com>
2021-02-27 20:38:01 -0600

    hide tasks func addition

--- src/App.js
+++ src/App.js
@@ -27,10 +27,15 @@ function App() {
     ]
   )
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))  
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} />:
+      'Your slate is empty, add tasks to continue!'}
     </div>
   );
 }
