1233731acd6173a0baf252e9d826a02cf137ce3d
Marcus-Johnson <marcusjdevelops@gmail.com>
2021-02-27 19:47:36 -0600

    tasks component addition + json addition

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1, 
+        text: 'SCRUM board review',
+        day: 'March 26 at 12:30pm',
+        reminder: true,
+    },
+    {
+        id: 2, 
+        text: 'AGILE methods review',
+        day: 'March 26 at 1:10pm',
+        reminder: true,
+    },
+    {
+        id: 3, 
+        text: 'Trello team review',
+        day: 'March 26 at 2:30pm',
+        reminder: false,
+    }
+] 
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
